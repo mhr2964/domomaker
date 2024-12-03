f3eb5446e09facaaeb9bbafb70eebf318f8cf740
mhr2964 <mhr2964@rit.edu>
2024-12-03 14:15:02 -0500

    test

--- server/app.js
+++ server/app.js
@@ -26,76 +26,74 @@ const router = require('./router.js');
 
 // connecting to a mongodb address.
 // either the one we made in Heroku or a fallback here
-const dbURI = process.env.MONGODB_URI || require('');
-
-// Connect called here.
-mongoose.connect(dbURI).catch((err) => {
-    if (err) {
-        console.log('Could not connect to db');
-        throw err;
-    }
-});
-
-const redisClient = redis.createClient({
-    url: process.env.REDISCLOUD_URL
-});
-
-redisClient.on('error', err => console.log('Redis Client Error', err));
-
-redisClient.connect().then(() => {
-    // get express mvc server object
-    const app = express();
-
-    app.use(express.json());
-
-    // just using helmet
-    app.use(helmet());
-
-    // '/assets' used as a static mirror to /hosted
-    app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-
-    // sets favicon file path
-    app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-
-    // initiate compression
-    app.use(compression());
-
-    // // parse form POST requests as 'application/x-www-form-urlencoded'
-    // app.use(bodyParser.urlencoded({ extended: false }));
-
-    // // parse json body requests
-    // app.use(bodyParser.json());
-
-    app.use(session({
-        key: 'sessionid',
-        store: new RedisStore({
-            client: redisClient
-        }),
-        secret: 'Domo Arigato',
-        resave: false,
-        saveUninitialized: false,
-    }));
-
-    // set up handlebars and then set it as our view engine
-    app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
-    app.set('view engine', 'handlebars');
-
-    // sets view file path
-    app.set('views', `${__dirname}/../views`);
-
-    // pass the app to the router object
-    router(app);
-
-    // listen on the port
-    app.listen(port, (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log(`On port ${port}`);
-    });
-});
+const dbURI = process.env.MONGODB_URI; // || require('');
 
 // port
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+// Connect called here.
+mongoose.connect(dbURI).catch((err) => {
+  if (err) {
+    console.log('Could not connect to db');
+    throw err;
+  }
+});
 
+const redisClient = redis.createClient({
+  url: process.env.REDISCLOUD_URL,
+});
+
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+
+redisClient.connect().then(() => {
+  // get express mvc server object
+  const app = express();
+
+  app.use(express.json());
+
+  // just using helmet
+  app.use(helmet());
+
+  // '/assets' used as a static mirror to /hosted
+  app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+
+  // sets favicon file path
+  app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+
+  // initiate compression
+  app.use(compression());
+
+  // // parse form POST requests as 'application/x-www-form-urlencoded'
+  // app.use(bodyParser.urlencoded({ extended: false }));
+
+  // // parse json body requests
+  // app.use(bodyParser.json());
+
+  app.use(session({
+    key: 'sessionid',
+    store: new RedisStore({
+      client: redisClient,
+    }),
+    secret: 'Domo Arigato',
+    resave: false,
+    saveUninitialized: false,
+  }));
+
+  // set up handlebars and then set it as our view engine
+  app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+  app.set('view engine', 'handlebars');
+
+  // sets view file path
+  app.set('views', `${__dirname}/../views`);
+
+  // pass the app to the router object
+  router(app);
+
+  // listen on the port
+  app.listen(port, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`On port ${port}`);
+  });
+});
