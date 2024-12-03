45170032e72af9301023c78beb3633389987ddbf
mhr2964 <mhr2964@rit.edu>
2024-12-03 03:43:14 -0500

    b!

--- server/app.js
+++ server/app.js
@@ -17,13 +17,14 @@ const expressHandlebars = require('express-handlebars');
 // helmet : security library for express
 const helmet = require('helmet');
 const session = require('express-session');
-
+const RedisStore = require('connect-redis');
+const redis = require('redis');
 // router.js import for mvc
 const router = require('./router.js');
 
 // connecting to a mongodb address.
 // either the one we made in Heroku or a fallback here
-const dbURI = process.env.MONGODB_URI || require('../.mongo-connection.js');
+const dbURI = process.env.MONGODB_URI || require('');
 
 // Connect called here.
 mongoose.connect(dbURI).catch((err) => {
