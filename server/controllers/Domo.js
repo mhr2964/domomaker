37557bea2a17997dd95fc6fa07adb555f03f0de3
mhr2964 <mhr2964@rit.edu>
2024-12-03 17:29:59 -0500

    Domo C Midpoint

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -4,13 +4,14 @@ const { Domo } = models;
 
 const makerPage = async (req, res) => {
   try {
+    console.log(req.session.account);
     const query = { owner: req.session.account._id };
     const docs = await Domo.find(query).select('name age').lean().exec();
 
     return res.render('app', { domos: docs });
   } catch (err) {
     console.log(err);
-    return res.statuc(500).json({ error: 'Error retrieving domos...' });
+    return res.status(500).json({ error: 'Error retrieving domos...' });
   }
 };
 
